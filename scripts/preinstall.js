9c732894b2c863fb6a6b3eb54cde1bebd9e93bbc
Luyu Cheng <chengluyu@live.cn>
2024-07-07 00:44:01 +0800

    fix(scripts): output the package.json

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -3,10 +3,11 @@ import fs from "fs";
 // Use the published version of the npm package when running on Cloudflare.
 if (typeof process.env.CLOUDFLARE !== "undefined") {
   console.log("Using published version of npm package.")
-  const pkg = fs.readFileSync("package.json", "utf8")
-    .replace(/"file:\.\.\/npm\/dist"/g, () => {
-      console.log("Found the local npm package.")
-      return "\"^0.0.1-alpha.20\"";
-    });
+  const original = fs.readFileSync("package.json", "utf8")
+  console.log("Current package.json: ")
+  const pkg = original.replace(/"file:\.\.\/npm\/dist"/g, () => {
+    console.log("Found the local npm package.")
+    return "\"^0.0.1-alpha.20\"";
+  });
   fs.writeFileSync("package.json", pkg);
 }
